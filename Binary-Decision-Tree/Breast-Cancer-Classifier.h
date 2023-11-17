4d21a5e70e4aa72fd8276b23b5bc9f8cb4ab25d6
Ryan Jennings <ryfoje2@gmail.com>
2023-11-17 18:14:12 -0500

    Added testing code for Classifier

--- Binary-Decision-Tree/Breast-Cancer-Classifier.h
+++ Binary-Decision-Tree/Breast-Cancer-Classifier.h
@@ -6,12 +6,19 @@
 class Classifier {
 
 private:
-	Node<Patient>* root;
+
+	// Create typedefs for Nodes as they will be often re-written
+	typedef Node<PatientInfo::Patient> Node_t;
+	typedef LeafNode<PatientInfo::Patient> LeafNode_t;
+
+	Node_t* root;
 
 public:
 
 	Classifier() {
-		root = new RootNode<Patient>();
+		using namespace PatientInfo;
+		root = new Node<Patient>([](Patient patient) {return false; });
+
 	};
 
 };
