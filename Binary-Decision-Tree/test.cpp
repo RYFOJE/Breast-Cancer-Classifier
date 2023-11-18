aa6dc1a764b47d65f84a70766e0ee1d41ca45260
Ryan Jennings <ryfoje2@gmail.com>
2023-11-17 20:12:46 -0500

    Added test

--- Binary-Decision-Tree/test.cpp
+++ Binary-Decision-Tree/test.cpp
@@ -1,9 +1,16 @@
 #include "Breast-Cancer-Classifier.h"
+#include <iostream>
 
 int main() {
 
-	Classifier classifier;
+	Attributes test_R(2, 2, 3, 3, 3, 3, 3, 3, 3);
+	Patient testPatient_R("Ryan Jennings", test_R);
 
 
+	Classifier classifierClass;
+
+	classifierClass.classify_patient(testPatient_R);
+	
+	std::cout << "Patient Classified to: " << testPatient_R.get_classification();
 
 }
